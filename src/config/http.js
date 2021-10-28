4d19cd186d2665084ae0509d8b50c88164703cf9
Moustapha <moustaphadiakhate1212@gmail.com>
2021-10-28 07:57:50 +0000

    [01] adding codes for social connect

--- src/config/http.js
+++ src/config/http.js
@@ -1,7 +1,6 @@
 
 import axios from 'axios';
 import {PROD_URL, DEV_URL, LOCAL_URL} from '@env'
-    console.log(DEV_URL);
 export const httpClient = axios.create({
     baseURL: DEV_URL,
     headers: {
